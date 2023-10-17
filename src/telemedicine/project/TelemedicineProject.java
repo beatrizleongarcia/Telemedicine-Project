d0561b66a0965a30f0fd7402ea0e3238bf63de6f
beatr <b.leon4@usp.ceu.es>
2023-10-17 14:53:47 +0200

    subiendo proyecto para todos

--- src/telemedicine/project/TelemedicineProject.java
+++ src/telemedicine/project/TelemedicineProject.java
@@ -17,7 +17,7 @@ public class TelemedicineProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        // funcionara???????????????
+        // funcionara???????
         
     }
     
