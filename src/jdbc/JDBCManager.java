0db9cdb373de6d1c97f65a59eb719220d4e44164
Ángel de la Nava Navarro <78914210+AngeldelaNava@users.noreply.github.com>
2023-11-09 12:28:08 +0100

    createTables() updated. Database tables created. They are empty for the moment

--- src/jdbc/JDBCManager.java
+++ src/jdbc/JDBCManager.java
@@ -18,37 +18,41 @@ public class JDBCManager implements Manager {
 
     private Connection c;
 
+    @Override
     public void connect() {
         try {
             Class.forName("org.sqlite.JDBC");
             //here we get the connection
-            this.c = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\maria\\OneDrive\\Documents\\NetBeansProjects\\Telemedicine-Project");
+            this.c = DriverManager.getConnection("jdbc:sqlite:jdbc:sqlite:./db/TelemedicineProject.db");
             c.createStatement().execute("PRAGMA foreign_keys=ON");
             //patient = new PatientManager(c);
             //ecg = new ECGManager(c);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
+    @Override
     public void disconnect() {
         try {
             c.close();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    @Override
     public void createTables() {
         try {
             Statement stmt = c.createStatement();
 
-            String sq1 = "CREATE TABLE IF NOT EXISTS patient " + "(id     INTEGER  PRIMARY KEY AUTOINCREMENT,"
+            String sq1 = "CREATE TABLE IF NOT EXISTS Patient " + "(id     INTEGER  PRIMARY KEY AUTOINCREMENT,"
                     + " name  TEXT   NOT NULL, " + " lastname  TEXT   NOT NULL, " + " email   TEXT NOT NULL, "
                     + " username  TEXT   NOT NULL, " + " password  BLOB   NOT NULL, " + " gender TEXT CHECK (gender = 'M' OR gender = 'F'), "
                     + " mac TEXT NOT NULL) "
                     + "CREATE TABLE IF NOT EXISTS ECG " + "(id     INTEGER  PRIMARY KEY AUTOINCREMENT, "
-                    + " observation TEXT NOT NULL, " + " patientId INTEGER NOT NULL, ";//Hay que ver como meter el arraylist en la base de datos, porque SQLite no soporta ese tipo de dato
+                    + " observation TEXT NOT NULL, " + "ecg BLOB NOT NULL,"
+                    + "patientId INTEGER REFERENCES Patient(id) ON UPDATE CASCADE ON DELETE CASCADE)";
             stmt.executeUpdate(sq1);
 
             //sq1 = "CREATE TABLE IF NOT EXISTS ECG "
